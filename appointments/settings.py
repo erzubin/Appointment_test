f819b8b49a06622dea918d31bc9e3caa3e084293
erzubin <erzubin@gmail.com>
2018-05-18 13:40:56 -0700

    changed database settings

--- appointments/settings.py
+++ appointments/settings.py
@@ -86,8 +86,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
